8db34b72854f1cfcbd8c4142c4f2b375db29562d
ibrahimasy11 <ibhmsy11@gmail.com>
2025-12-16 13:17:21 -0500

    first push

--- src/1-media.js
+++ src/1-media.js
@@ -1,6 +1,18 @@
 // Problem 1: Inheritance and Polymorphism - Media Player
 // Implement your MediaItem, Song, Podcast, and Audiobook classes below
 class MediaItem {
+    constructor (title , duration){
+        this.title = title;
+        this.duration = duration;
+    }
+
+    play (){
+        return `PLaying: ${this.tilte}`
+    }
+
+    getFormattedDuration (){
+        
+    }
 
 }
 
--- src/2-team.js
+++ src/2-team.js
@@ -3,11 +3,67 @@
 
 class Player {
 
+    constructor(name, position, jerseyNumber){
+        this.name = name;
+        this.position = position;
+        this.jerseyNumber = jerseyNumber;
+    }
+
 }
 
 class Team {
+  #starters = [];
+  #bench = [];
 
+  constructor(name) {
+    this.name = name;
+  }
+
+  getStarters() {
+    return this.#starters;
+  }
+
+  getBench() {
+    return this.#bench;
+  }
+
+  addBenchPlayer(player) {
+    this.#bench.push(player);
+  }
+
+  getPlayerCount() {
+    return this.#starters.length + this.#bench.length;
+  }
+
+  moveToBench(name) {
+    const currentPlayer = this.#starters.find(
+      player => player.name === name
+    );
+
+    if (!currentPlayer) return false;
+
+    const index = this.#starters.indexOf(currentPlayer);
+    this.#starters.splice(index, 1);
+    this.#bench.push(currentPlayer);
+
+    return true;
+  }
+
+  moveToStarters(name) {
+  const currentPlayer = this.#bench.find(
+    player => player.name === name
+  );
+
+  if (!currentPlayer) return false;
+
+  const index = this.#bench.indexOf(currentPlayer);
+  this.#bench.splice(index, 1);
+  this.#starters.push(currentPlayer);
+
+  return true;
 }
+
+};
 
 module.exports = { Player, Team };
 
